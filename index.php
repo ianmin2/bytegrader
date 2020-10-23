46bd5c0fc4be353d767c419dabf04d9d9201cff4
Ian Innocent <ianmin2@live.com>
2020-10-23 21:42:51 +0100

    Fixed broken composer dependency loader

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 //@ ALLOW CORS REQUESTS
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST,GET,OPTIONS");
-header("Content-Type:application/json");
-// echo phpinfo();
+ header("Access-Control-Allow-Origin: *");
+ header("Access-Control-Allow-Headers: *");
+ header("Access-Control-Allow-Methods: POST,GET,OPTIONS");
+ header("Content-Type:application/json");
+
 // include __DIR__ . "/php-router/index.php";
-include __DIR__ . "/framify/index.php";
+ include __DIR__ . "/framify/index.php";
+
+//include __DIR__."/framify/request_client.php";
